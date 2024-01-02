bd47584eb0ed9a667ffd9010a4472a7c59484fd3
Allen Liu <JingkunLiu2025@u.northwestern.edu>
2024-01-02 11:40:12 -0600

    update all

--- Assignments/Assignment 1/HelloWorld.c
+++ Assignments/Assignment 1/HelloWorld.c
@@ -1,5 +1,22 @@
+/**
+ * @file HelloWorld.c
+ * @author Allen Liu (jingkunliu2025@u.northwestern.edu)
+ * @brief This file is to display the "Hello World." on the command output.
+ * @version 0.1
+ * @date 2024-01-02
+ *
+ * @copyright Copyright (c) 2024
+ *
+ */
 #include <stdio.h>
 
+/**
+ * @brief The main function of the program.
+ *
+ * @param argc number of the arguments
+ * @param argv the value of the arguments
+ * @return int error code.
+ */
 int main(int argc, char **argv)
 {
     printf("Hello World.\n");
